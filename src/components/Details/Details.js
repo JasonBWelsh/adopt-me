a423e8dd4d54c3c8a8039a23fbacbd7166726d89
Jason Welsh <jasonbwelsh@gmail.com>
2020-08-07 17:07:55 -0400

    WIP. API request is failing

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyledDetails } from './StyledDetails.js';
+import pet from '@frontendmasters/pet';
 
-function Details(props) {
-  return (
-    <StyledDetails>
-      <h3>Styled Details</h3>
-      {
-        <pre>
-          <code>{JSON.stringify(props, null, 4)}</code>
-        </pre>
-      }
-    </StyledDetails>
-  );
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      animal: '',
+      location: '',
+      description: '',
+      media: [],
+      loading: true,
+      error: false,
+    };
+  }
+
+  componentDidMount() {
+    console.log('DRD test log `pet`', pet);
+    pet
+      .animal(this.props.id)
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false,
+          error: false,
+        });
+      })
+      .catch((error) => {
+        console.log('DRD :( ==>', error);
+        this.setState({ ...this.state, error: true, loading: false });
+      });
+
+    try {
+    } catch (error) {
+      console.log('DRD fuck!', error);
+    }
+  }
+
+  render() {
+    return <>{this.state.loading ? <h1>Loading...</h1> : <div>Loaded</div>}</>;
+  }
 }
 
 export default Details;
